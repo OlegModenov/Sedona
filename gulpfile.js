8e00ef952e29ae117d37a0653052921fa9ec727b
Oleg <olegmodenov@gmail.com>
2019-08-10 23:35:48 +0600

    Минимизирован css и оптимизированы картинки с помощью gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -14,8 +18,9 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
-    ]))
+      autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -35,3 +40,20 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("images", function () {
+ return gulp.src("source/img/**/*.{png,jpg,svg}")
+ .pipe(imagemin([
+ imagemin.optipng({optimizationLevel: 3}),
+ imagemin.jpegtran({progressive: true}),
+ imagemin.svgo()
+ ]))
+ .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+ return gulp.src("source/img/**/*.{png,jpg}")
+ .pipe(webp({quality: 90}))
+ .pipe(gulp.dest("source/img"));
+});
+
