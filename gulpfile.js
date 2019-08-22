32c83315b504889fad8d6857f862c80f68fc1d7b
Oleg <olegmodenov@gmail.com>
2019-08-22 22:20:50 +0600

    Решены проблемы с переполнением

--- gulpfile.js
+++ gulpfile.js
@@ -119,6 +119,7 @@ gulp.task("server", function () {
   gulp.watch("source/img/*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/img/*.png", gulp.series("copy", "refresh"));
 });
 
 gulp.task("start", gulp.series("build", "server"));
